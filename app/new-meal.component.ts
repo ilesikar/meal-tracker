94f98126b30e8f104bd8c6bd0898625b6cbbbdf9
Ian Lesikar <ilesikar@gmail.com>
2016-06-03 13:43:44 -0700

    updated styles.scss, added new-meal.component.ts

---
+++ app/new-meal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from 'angular2/core';
+import { Meal } from './meal.model';
+
+@Component({
+  selector: 'new-meal',
+  template: `
+  <h3>Blank template</h3>
+  `
+})
+
+export class NewMealComponent
