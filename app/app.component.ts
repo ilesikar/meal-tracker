9a707fbf46f307239dd2cd22d5556f56bff1a164
Ian Lesikar <ilesikar@gmail.com>
2016-06-03 09:43:57 -0700

    clean up app.component.ts, main.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,30 +1,27 @@
 import { Component } from 'angular2/core';
+import {} from '';
 
 @Component({
   selector: 'my-app',
   template: `
     <div class="container">
-      <h1>Tap Room!</h1>
-      <h3>{{ rename.description }}</h3>
+      <h1>Meal Tracker!</h1>
+      <h3>{{ Meal.name }}</h3>
     </div>
   `
 })
-
+//
 export class AppComponent {
-  public renames: Rename[];  // Task[] (or Array<Task>) identifies tasks as an array of Task objects
-  constructor(){
-    this.renames = [
-      new Rename("Create To-Do List app.", 0),
-      new Rename("Learn Kung Fu.", 1),
-      new Rename("Rewatch all the Lord of the Rings movies.", 2),
-      new Rename("Do the laundry.", 3)
-    ];
-  }
+  // public meals: Meal[];  // Task[] (or Array<Task>) identifies tasks as an array of Task objects
+  // constructor(){
+  //   this.meals = [
+  //     new Meal("Pizza.", "3 slices", 600),
+  //     new Meal("Banana", "whole", 100)
+  //   ];
+  // }
 }
 
-export class Rename {
+export class MealTracker {
   public done: boolean = false;
-  constructor(public description: string, public id: number) {
-
-  }
+  constructor(public name: string, public description: string, public calories: number) {}
 }
