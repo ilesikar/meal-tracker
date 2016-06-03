c749e6f2a302cccd779dec2d569eac92edaf9845
Ian Lesikar <ilesikar@gmail.com>
2016-06-03 10:12:29 -0700

    updated app.component.ts, main.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,27 +1,29 @@
-import { Component } from 'angular2/core';
-import {} from '';
+import { Component, EventEmitter } from 'angular2/core';
+// import {} from '';
 
 @Component({
   selector: 'my-app',
   template: `
     <div class="container">
       <h1>Meal Tracker!</h1>
-      <h3>{{ Meal.name }}</h3>
+      <h3 *ngFor="#meal of meals" (click)="mealWasSelected(meal)">{{ meal.name }}</h3>
     </div>
   `
 })
-//
+
 export class AppComponent {
-  // public meals: Meal[];  // Task[] (or Array<Task>) identifies tasks as an array of Task objects
-  // constructor(){
-  //   this.meals = [
-  //     new Meal("Pizza.", "3 slices", 600),
-  //     new Meal("Banana", "whole", 100)
-  //   ];
-  // }
+  public meals: Meal[];  // Task[] (or Array<Task>) identifies tasks as an array of Task objects
+  constructor(){
+    this.meals = [
+      new Meal("Pizza", "3 slices", 600),
+      new Meal("Banana", "whole", 100)
+    ];
+  }
+  mealWasSelected(clickedMeal: Meal): void {
+    console.log(clickedMeal);
+  }
 }
 
-export class MealTracker {
-  public done: boolean = false;
+export class Meal {
   constructor(public name: string, public description: string, public calories: number) {}
 }
