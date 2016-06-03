d349c9779b621f5960f38e789878ec6d69fe1767
Ian Lesikar <ilesikar@gmail.com>
2016-06-03 11:32:02 -0700

    edit-meal-details.componrnt.ts updated with basic component logic

--- app/edit-meal-details.component.ts
+++ app/edit-meal-details.component.ts
@@ -1,1 +1,13 @@
 import { Component } from 'angular2/core';
+import { Meal } from './meal.model';
+
+@Component({
+  selector: 'edit-meal-details',
+  inputs: ['meal'],
+  template `
+  `
+})
+
+export class EditMealDetails.Component {
+  public meal: Meal;
+}
